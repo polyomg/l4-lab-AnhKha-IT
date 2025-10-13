22444fb771a1eeae5fd05803680411fe0050f8ce
AnhKha-IT <khalha927@gmail.com>
2025-10-14 06:45:09 +0700

    TS01294 - Lê Hoàng Anh Kha - Lab5

--- Lab5/src/test/java/poly/edu/Ts01294Lab5ApplicationTests.java
+++ Lab5/src/test/java/poly/edu/Ts01294Lab5ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.poly.lab5;
+package poly.edu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab5ApplicationTests {
+class Ts01294Lab5ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
