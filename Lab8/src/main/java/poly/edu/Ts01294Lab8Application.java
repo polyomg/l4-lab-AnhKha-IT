c91ff50ba4c7d08fabb2a154098dabaa7faa0c00
AnhKha-IT <khalha927@gmail.com>
2025-10-23 22:48:30 +0700

    TS01294 - Lê Hoàng Anh Kha - Lab8

---
+++ Lab8/src/main/java/poly/edu/Ts01294Lab8Application.java
@@ -0,0 +1,15 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Ts01294Lab8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ts01294Lab8Application.class, args);
+	}
+
+}
--- Lab8/src/test/java/poly/edu/Ts01294Lab8ApplicationTests.java
+++ Lab8/src/test/java/poly/edu/Ts01294Lab8ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.poly.lab8;
+package poly.edu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab8ApplicationTests {
+class Ts01294Lab8ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
